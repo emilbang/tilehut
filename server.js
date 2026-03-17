5fd67a0b063f93dcdedbe4c69a4e9451a79af093
emilbang <emil.m.bang@gmail.com>
2026-03-17 10:24:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ var routeHandlers = {
 
         const basename = file.split('.').slice(0, -1).join('.');
         
-        info[basename] = {"meta": "https://th.frb-data.dk/" + basename + "/meta.json", "interactive": "https://th.frb-data.dk/" + basename + "/map"};
+        info[basename] = {"meta": "https://th1.frb-data.dk/" + basename + "/meta.json", "interactive": "https://th1.frb-data.dk/" + basename + "/map"};
       });
       res.json(info);
     });
